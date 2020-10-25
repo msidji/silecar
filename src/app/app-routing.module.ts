c953a46d8c037e76aaf9d71e7b40b3b7234bcb96
msidji <milos.sidji@gmail.com>
2020-10-25 18:42:00 +0100

    Update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/magic', pathMatch: 'full' },
+  { path: 'magic', component: HomeComponent },
 ];
 
 @NgModule({
